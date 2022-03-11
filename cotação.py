a8a23e81aba1c23a83e2d34f12e6ef88c307ed3a
Gustavo Roos <gustavohroos@gmail.com>
2022-03-11 18:11:17 -0300

    uploaded

---
+++ cotação.py
@@ -0,0 +1,22 @@
+import requests
+import time
+import datetime
+
+url_api_conversor = 'https://economia.awesomeapi.com.br/last/USD-BRL'
+
+def get_cotacao():
+    response = requests.get(url_api_conversor)
+    data = response.json()
+    cotacao = float(data['USDBRL'].get('bid'))
+    return cotacao
+
+def rodar():
+    while True:
+        cotacao = get_cotacao()
+        now = datetime.datetime.now()
+        date_time = now.strftime("%m/%d/%y %H:%M:%S")
+        print(f'{date_time} -=- Cotação: R${cotacao:.4f}')
+        time.sleep(1)
+
+if __name__ == '__main__':
+    rodar()
